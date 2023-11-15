fedfee290396a2c34c5b83cc02c222b8c00d37d3
Ikrammm1 <ikramunyil11@gmail.com>
2023-11-15 19:25:15 +0700

    Add Parent Module

---
+++ app/src/main/java/com/wisnu/evoting/Ui/Hasil/HasilActivity.kt
@@ -0,0 +1,13 @@
+package com.wisnu.evoting.Ui.Hasil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wisnu.evoting.R
+
+class HasilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil)
+    }
+}
